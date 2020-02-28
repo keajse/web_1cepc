20de553c8c228eb3ec4cd2bd0ba0c8bc48ef9f81
Laura Serrano <lajusega@gmail.com>
2020-02-27 19:00:17 -0500

    Avance

--- Web_Lajusega/Clase_27_02_2020/index.php
+++ Web_Lajusega/Clase_27_02_2020/index.php
@@ -1,18 +1,31 @@
 <?php
 
+    echo "Paz y bien ";
+    $numero = 5;
+    //Para declarar una variable se debe anteponer el signo pesos, siempre que se llame.
+
+    echo $numero;
+
 ?>
 
-<!DOCTYPE html>
+<!-- <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Ejemplo</title>
 </head>
 <body>
     <head>
-    <h1>Index</h1>
+        <h1>Index</h1>
     </head>
+
+    <section>
     
+    </section>
+    
+    <footer>
+        Created by Keajse - 2020
+    </footer>
 </body>
-</html>
+</html> -->
