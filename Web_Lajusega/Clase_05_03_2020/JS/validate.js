e0aee7bd264055d1c256926ffa9dd13d3a9d5e27
Laura Serrano <lajusega@gmail.com>
2020-03-05 19:45:25 -0500

    Clase del 03/05/2020

---
+++ Web_Lajusega/Clase_05_03_2020/JS/validate.js
@@ -0,0 +1,10 @@
+function validate() {
+    let email = document.getElementById('email').value;
+    
+    let password = document.getElementById('password').value;
+
+    alert('Datos: ' + email + " "+ password)
+
+    return false;
+}
+
